888d723e4bf94943039fe1022032d4c4d6c22727
Fernando Cassio <fernandocassiodev@gmail.com>
2024-09-16 13:53:50 -0300

    first commit

---
+++ src/main/java/com/blogpessoal/blogpessoaljava/BlogpessoaljavaApplication.java
@@ -0,0 +1,13 @@
+package com.blogpessoal.blogpessoaljava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogpessoaljavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogpessoaljavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogpessoal/blogpessoaljava/BlogpessoaljavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogpessoal.blogpessoaljava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogpessoaljavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
