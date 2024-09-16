91fb5ca318e57614a3e1a9250c8ea47f95cf06b5
Fernando Cassio <fernandocassiodev@gmail.com>
2024-09-16 20:38:01 -0300

    feat: cria a entidade Postagem

--- src/main/java/com/blogpessoal/blogpessoaljava/BlogpessoaljavaApplication.java
+++ src/main/java/com/blogpessoal/blogpessoaljava/BlogpessoaljavaApplication.java
@@ -3,7 +3,7 @@ package com.blogpessoal.blogpessoaljava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.blogpessoal")
 public class BlogpessoaljavaApplication {
 
 	public static void main(String[] args) {
