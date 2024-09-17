6f0172de388ab0717598a30add69debb1ce18095
Fernando Cassio <fernandocassiodev@gmail.com>
2024-09-17 16:04:26 -0300

    feat: CRUD de postagem feito

--- src/main/java/com/blogpessoal/blogpessoaljava/repository/PostagemRepository.java
+++ src/main/java/com/blogpessoal/blogpessoaljava/repository/PostagemRepository.java
@@ -1,7 +1,12 @@
 package com.blogpessoal.blogpessoaljava.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.blogpessoal.blogpessoaljava.model.Postagem;
 
-public interface PostagemRepository extends JpaRepository<Postagem, Long>{}
+public interface PostagemRepository extends JpaRepository<Postagem, Long>{
+  public List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo")String titulo);
+}
